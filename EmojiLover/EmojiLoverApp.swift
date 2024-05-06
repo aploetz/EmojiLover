0c7a5566bd0a4ab91f7f0389f4aba6341dfabfd5
Aaron Ploetz <aaron.ploetz@aarons-mbp.mynetworksettings.com>
2024-05-05 21:35:41 -0500

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Aaron Ploetz on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
